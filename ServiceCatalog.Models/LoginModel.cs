f91a72ee32c2fcdeb0bd6f0aa7db069cbe73b5e7
Kanyarat123 <the.mastermook82@gmail.com>
2026-04-03 17:20:27 +0700

    Login and Manage User

---
+++ ServiceCatalog.Models/LoginModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ServiceCatalog.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        //[EmailAddress]
+        [StringLength(150)]
+        [Display(Name = "User: ")]
+        public string Usre { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(150, MinimumLength = 2)]
+        [Display(Name = "Password: ")]
+        public string Password { get; set; }
+    }
+}
---
+++ ServiceCatalog/Controllers/LoginController.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Runtime.InteropServices;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.DirectoryServices;
+using ServiceCatalog.Library;
+
+namespace ServiceCatalog.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            if (this.Session["UserType"] == null)
+                this.Session["UserType"] = "";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string User, string Password)
+        {
+            if (string.IsNullOrWhiteSpace(User) || string.IsNullOrWhiteSpace(Password))
+            {
+                ModelState.AddModelError("", "Please enter username and password.");
+                return View();
+            }
+
+            string userTrim = User.ToTrim();
+            string passTrim = Password.ToTrim();
+            string UserType = string.Empty;
+
+            var connectionString = ConfigurationManager.ConnectionStrings["ServiceCatalogDB"].ConnectionString;
+
+            try // ✅ Case 1: Login ผ่าน AD ได้
+            {
+                DirectoryEntry entry = new DirectoryEntry(
+                    "LDAP://ADSRV2016-01/dc=Automotive,dc=com", userTrim, passTrim);
+                DirectorySearcher search = new DirectorySearcher(entry);
+                search.Filter = "(SAMAccountName=" + userTrim + ")";
+                search.PropertiesToLoad.Add("cn");
+                search.PropertiesToLoad.Add("department");
+
+                SearchResult result = search.FindOne();
+
+                if (result == null)
+                {
+                    ModelState.AddModelError("", "Login details are wrong.");
+                    return View();
+                }
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    // ✅ JOIN UsrTbl + v_ADUser เช็คว่ามี User ใน AD และ UsrTbl
+                    string sql = @"SELECT Usr.UserType, Usr.Slmcod,
+                                          Ad.Department, Ad.Company,
+                                          Ad.FName, Ad.LName, Ad.mail,
+                                          Ad.Position, Ad.EmpID
+                                   FROM UsrTbl Usr
+                                   INNER JOIN [LIP].[dbo].[v_ADUser] Ad 
+                                          ON Ad.LogInName = Usr.UserID
+                                   WHERE Usr.UserID = @inUser
+                                   AND Usr.Password IS NULL";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@inUser", userTrim);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.HasRows) // ✅ มีใน UsrTbl และ v_ADUser
+                            {
+                                while (reader.Read())
+                                {
+                                    this.Session["UserAD"] = "YES";
+                                    this.Session["UserID"] = userTrim;
+                                    this.Session["UserType"] = reader["UserType"].ToString();
+                                    this.Session["Department"] = reader["Department"].ToString();
+                                    this.Session["Company"] = reader["Company"].ToString();
+                                    this.Session["SLMCOD"] = reader["Slmcod"].ToString();
+                                    this.Session["FName"] = reader["FName"].ToString();
+                                    this.Session["LName"] = reader["LName"].ToString();
+                                    this.Session["Email"] = reader["mail"].ToString();
+                                    this.Session["Position"] = reader["Position"].ToString();
+                                    this.Session["EmpID"] = reader["EmpID"].ToString();
+                                    UserType = reader["UserType"].ToString();
+                                }
+                            }
+                            else // ✅ ไม่มีใน UsrTbl — เช็ค Department จาก AD โดยตรง
+                            {
+                                reader.Close();
+
+                                string sqlAd = @"SELECT Department 
+                                                 FROM [LIP].[dbo].[v_ADUser] 
+                                                 WHERE LogInName = @inUser";
+
+                                using (SqlCommand cmdAd = new SqlCommand(sqlAd, conn))
+                                {
+                                    cmdAd.Parameters.AddWithValue("@inUser", userTrim);
+
+                                    using (SqlDataReader readerAd = cmdAd.ExecuteReader())
+                                    {
+                                        if (readerAd.HasRows && readerAd.Read())
+                                        {
+                                            string dept = readerAd["Department"].ToString();
+
+                                            if (dept == "MIS")
+                                            {
+                                                this.Session["UserAD"] = "YES";
+                                                this.Session["UserID"] = userTrim;
+                                                this.Session["UserType"] = "1"; // MIS = Admin
+                                                this.Session["Department"] = dept;
+                                                UserType = "1";
+                                            }
+                                            else
+                                            {
+                                                ModelState.AddModelError("",
+                                                    "You don't have permission, Please contact admin.");
+                                                return View();
+                                            }
+                                        }
+                                        else
+                                        {
+                                            ModelState.AddModelError("", "Login details are wrong.");
+                                            return View();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                return RedirectByUserType(UserType);
+            }
+            catch (COMException) // ✅ Case 2: ไม่มีใน AD — เช็คจาก UsrTbl + v_ADUser
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string sql = @"SELECT Usr.UserType, Usr.Slmcod,
+                                          Ad.Department, Ad.Company,
+                                          Ad.FName, Ad.LName, Ad.mail,
+                                          Ad.Position, Ad.EmpID
+                                   FROM UsrTbl Usr
+                                   LEFT JOIN [LIP].[dbo].[v_ADUser] Ad 
+                                          ON Ad.LogInName = Usr.UserID
+                                   WHERE Usr.UserID = @inUser
+                                   AND [dbo].F_decrypt(Usr.[Password]) = @inPassword";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@inUser", userTrim);
+                        cmd.Parameters.AddWithValue("@inPassword", passTrim);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                while (reader.Read())
+                                {
+                                    this.Session["UserAD"] = "NO";
+                                    this.Session["UserID"] = userTrim;
+                                    this.Session["UserType"] = reader["UserType"].ToString();
+                                    this.Session["Department"] = reader["Department"].ToString();
+                                    this.Session["Company"] = reader["Company"].ToString();
+                                    this.Session["SLMCOD"] = reader["Slmcod"].ToString();
+                                    this.Session["FName"] = reader["FName"].ToString();
+                                    this.Session["LName"] = reader["LName"].ToString();
+                                    this.Session["Email"] = reader["mail"].ToString();
+                                    this.Session["Position"] = reader["Position"].ToString();
+                                    this.Session["EmpID"] = reader["EmpID"].ToString();
+                                    UserType = reader["UserType"].ToString();
+                                }
+                            }
+                            else
+                            {
+                                ModelState.AddModelError("", "Login details are wrong.");
+                                return View();
+                            }
+                        }
+                    }
+                }
+
+                return RedirectByUserType(UserType);
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "An error occurred. Please try again.");
+                return View();
+            }
+        }
+
+        private ActionResult RedirectByUserType(string userType)
+        {
+            if (userType == "1" || userType == "2" || userType == "3")
+            {
+                FormsAuthentication.SetAuthCookie(
+                    this.Session["UserID"].ToString(), false);
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "You don't have permission, Please contact admin.");
+            return View("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            Session.Abandon();
+            FormsAuthentication.SignOut();
+            return Json(new { status = "success" });
+        }
+    }
+}
