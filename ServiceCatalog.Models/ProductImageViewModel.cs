437daddc0fe323957cfb2c99badcde342ae32a1e
Kanyarat123 <the.mastermook82@gmail.com>
2026-04-22 15:28:34 +0700

    Add ImageViewModel

---
+++ ServiceCatalog.Models/ProductImageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCatalog.Models
+{
+    public class ProductImageViewModel
+    {
+        public string Company { get; set; }
+        public string Stkcode { get; set; }
+        public string Stkdesc { get; set; }
+        public string BrandId { get; set; }
+        public string BrandName { get; set; }
+        public string Category { get; set; }
+        public string ProductCode { get; set; }
+        public string ProductName { get; set; }
+        public int SeqImage { get; set; }
+        public string Filename { get; set; }
+        public string Url { get; set; }
+    }
+}
