b2f90e334a5540af7caa2482019c56340923d967
Kanyarat123 <the.mastermook82@gmail.com>
2026-07-20 11:14:33 +0700

    Up

---
+++ ServiceCatalog.Models/ProductGroupModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCatalog.Models
+{
+    public class CatProductGroup
+    {
+        public string Company { get; set; }
+        public string ProductGroup { get; set; }
+        public string ProductLine { get; set; }
+
+    }
+}
